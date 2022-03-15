63411116be3f774baccf25f5a4e4ad271cecd859
Emmaniih <imanuselam@gmail.com>
2022-03-15 14:47:27 -0700

    Functions, Nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 /**
  * print_to_98 - a function that prints all natural numbers from n to 98
  * user input's number prints to 98, regardless < 98 or > 98
- * num: number input
+ * @num: number input
  * Return: Always 0 (Success)
  */
 void print_to_98(int num)
